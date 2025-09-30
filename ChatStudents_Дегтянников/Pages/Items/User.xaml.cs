4a0274d1199bc604e8cb7e6a15da5c94bff0a857
Djfjfj2006@outlook.com <Djfjfj2006@outlook.com>
2025-09-30 22:24:11 +0500

    Разработанный пользовательский элемент User. Дегтянников

---
+++ ChatStudents_Дегтянников/Pages/Items/User.xaml.cs
@@ -0,0 +1,41 @@
+﻿using ChatStudents_Дегтянников.Classes.Common;
+using ChatStudents_Дегтянников.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace ChatStudents_Дегтянников.Pages.Items
+{
+    /// <summary>
+    /// Логика взаимодействия для User.xaml
+    /// </summary>
+    public partial class User : UserControl
+    {
+        User user;
+        Main main;
+        public User(Users user,Main main)
+        {
+            InitializeComponent();
+            this.user = user;
+            this.main = main;
+            imgUser.Source = BitmapFromArrayByte.LoadImage(user.Photo);
+            FIO.Content = user.ToFIO();
+        }
+
+        private void SelectChat(object sender, System.Windows.Input.MouseButtonEventArgs e)
+        {
+            main.SelectUser(user);
+        }
+    }
+}
