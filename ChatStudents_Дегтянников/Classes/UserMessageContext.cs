c92548832e96d958417bc2e607d86bd1ab9bf6af
Djfjfj2006@outlook.com <Djfjfj2006@outlook.com>
2025-09-30 20:47:15 +0500

    Разработанные контексты данных.Дегтянников

---
+++ ChatStudents_Дегтянников/Classes/UserMessageContext.cs
@@ -0,0 +1,24 @@
+﻿using ChatStudents_Дегтянников.Models;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Storage;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ChatStudents_Дегтянников.Classes
+{
+    public class UserMessageContext: DbContext
+    {
+        public DbSet<Messages> Messages { get; set; }
+        public DbSet<Users> Users { get; set; } 
+        public UserMessageContext() =>
+            Database.EnsureCreated();
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            //optionsBuilder.UseSqlServer(Config.config);
+            optionsBuilder.UseMySql(Common.Config.ConnectionConfig, Common.Config.Version);
+        }
+    }
+}
