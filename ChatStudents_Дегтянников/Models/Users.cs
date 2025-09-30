4683a75787a1869524e5ea472a2374fd902a2cdf
Djfjfj2006@outlook.com <Djfjfj2006@outlook.com>
2025-09-30 20:27:02 +0500

    Разработанные модели. Дегтянников

---
+++ ChatStudents_Дегтянников/Models/Users.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ChatStudents_Дегтянников.Models
+{
+    public class Users
+    {
+        public int Id { get; set; } 
+        public string LastName { get; set; }
+        public string FirstName { get; set; }
+        public string Surname { get; set; }
+        public byte[] Photo { get; set; }
+        public Users(string LastName,string FirstName,string Surname, byte[] Photo)
+        {
+            this.LastName = LastName;
+            this.FirstName = FirstName;
+            this.Surname = Surname;
+            this.Photo = Photo;
+        }
+        public string ToFIO()
+        {
+            return $"{LastName} {FirstName} {Surname}";
+        }
+    }
+}
