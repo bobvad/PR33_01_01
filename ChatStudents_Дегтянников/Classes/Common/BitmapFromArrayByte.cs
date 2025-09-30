fcb29d6762e9f17a261aa3fe4d41c70043366965
Djfjfj2006@outlook.com <Djfjfj2006@outlook.com>
2025-09-30 20:43:43 +0500

    Разработанные общие классы. Дегтянников

---
+++ ChatStudents_Дегтянников/Classes/Common/BitmapFromArrayByte.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Media.Imaging;
+
+namespace ChatStudents_Дегтянников.Classes.Common
+{
+    public class BitmapFromArrayByte
+    {
+        public static BitmapImage LoadImage(byte[] imagedata)
+        {
+            BitmapImage Image = new BitmapImage();
+            using(var Stream = new MemoryStream(imagedata))
+            {
+                Stream.Position = 0;
+                Image.BeginInit();
+                Image.CreateOptions = BitmapCreateOptions.PreservePixelFormat;
+                Image.CacheOption = BitmapCacheOption.OnLoad;
+                Image.UriSource = null;
+                Image.StreamSource = Stream;
+                Image.EndInit();
+            }
+            Image.Freeze();
+            return Image;
+        }
+    }
+}
---
+++ ChatStudents_Дегтянников/Classes/Common/Config.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.EntityFrameworkCore;
+namespace ChatStudents_Дегтянников.Classes.Common
+{
+    public class Config
+    {
+        //public static readonly string config = "Server=student.permaviat.ru;Trusted_Connection=No;DataBase=;User=;PWD=;";
+        public static string ConnectionConfig = "server=127.0.0.1;uid=root;pwd=; database=ChatMessage";
+        public static MySqlServerVersion Version = new MySqlServerVersion(new Version(8, 0, 11));
+    }
+}
